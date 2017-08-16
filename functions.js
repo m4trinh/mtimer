7139ee4c9bc8235ffed2e5978a7b8ac90227e679
Michael Trinh <michaelstrinhs@gmail.com>
2017-08-15 23:42:03 -0400

    Add files via upload

--- functions.js
+++ functions.js
@@ -5,10 +5,15 @@ var totalmSecond = 0;
 var mSecond = 0;
 var second = 0;
 var minute = 0;
-var hour = 0;
 var moves = ['R\'','L\'','U\'','D\'','F\'','B\'','R','L','U','D','F','B'];
+var totalSolves = 0;
+var avgThree = 0;
+var avgFive = 0;
+var avgTen = 0;
+var times = [];
+
 function start () {
-  stop();
+  
   //If the time is less than 10 milliseconds, it will use 10 milliseconds...this changes up the math....
   timerInterval = setInterval(changeValue, 10);  
 }
@@ -16,24 +21,61 @@ function start () {
 function changeValue() {
 
 	totalmSecond += 10;
-	minute = Math.floor(totalmSecond/60000.0);
-	second = Math.floor((totalmSecond - (minute*60000))/1000.0);
-	mSecond = totalmSecond-(second*1000) - (minute*60000);
-
-	tempString = (minute == 0?"":minute + ":") + (second == 0?"":second + ":") + (mSecond == 0?"":mSecond);
-	document.getElementById("time").innerHTML = tempString;
+	
+	document.getElementById("time").innerHTML = formatTime(totalmSecond);
   	
 
 }
 
+function formatTime(totalmSecond){
+  minute = Math.floor(totalmSecond/60000.0);
+  second = Math.floor((totalmSecond - (minute*60000))/1000.0);
+  mSecond = totalmSecond-(second*1000) - (minute*60000);
+
+  tempString = (minute == 0?"":minute + ":") + (second == 0?"":second + ":") + (mSecond == 0?"":mSecond);
+  return tempString;
+
+}
+
+
 var stop = function () {
+
 	generateScramble(); 
+  times.push(totalmSecond);
   clearInterval(timerInterval);
   totalmSecond =0;
   mSecond = 0;
   second = 0;
   minute =0;
-  hour = 0;
+  totalSolves++;
+  var tempTotal = 0;
+  if(times.length >= 3){
+   
+    for(i = times.length - 1;i > times.length - 4;i--){
+        tempTotal += times[i];
+    }
+    tempTotal /= 3.0;
+    document.getElementById("avg3").innerHTML = "Avg 3: " + formatTime(Math.round(tempTotal));
+  }
+  if(times.length >= 5){
+    tempTotal = 0;
+     for(i = times.length - 1;i > times.length - 6;i--){
+        tempTotal += times[i];
+    }
+    tempTotal /= 5.0;
+    document.getElementById("avg5").innerHTML = "Avg 5: " + formatTime(Math.round(tempTotal));
+  }
+  if(times.length >= 10){
+    tempTotal = 0;
+     for(i = times.length - 1;i > times.length - 11;i--){
+        tempTotal += times[i];
+    }
+    tempTotal /= 10.0;
+    document.getElementById("avg10").innerHTML = "Avg 10: " + formatTime(Math.round(tempTotal));
+
+  }
+  
+
 }
 
 
@@ -42,12 +84,13 @@ document.onkeydown = function (e) {
     if (e.keyCode == 32) {
     	
     	if(isRunning){
-  		isRunning = false;
-        start();
-
+        stop();
+  		  isRunning = false;
+        
     	}else{
+        start();
     		isRunning = true;
-    		stop();
+    		
 
     	}
     	
@@ -55,7 +98,19 @@ document.onkeydown = function (e) {
     }
 }
 
+function generateScramble(){
+  var myString = ""; 
+  for(i = 0;i < 20;i++){
+
+    myString += moves[Math.round(Math.random()*10)+(Math.random()<0.5?1:0)] + ' ';
+
+  }
+  document.getElementById("scramble").innerHTML = myString;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+    generateScramble();
+
     var link = document.getElementById('execute');
     // onClick's logic below:
     link.addEventListener('click', function() {
@@ -64,15 +119,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
   
 
-function generateScramble(){
-	var myString = ""; 
-	for(i = 0;i < 20;i++){
-
-		myString += moves[Math.round(Math.random()*10)+(Math.random()<0.5?1:0)] + ' ';
-
-	}
-	document.getElementById("scramble").innerHTML = myString;
-}
 
 
 }
